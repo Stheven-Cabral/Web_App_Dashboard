e7957973080ee947a1ed7dd418292bdf7cf18ec7
Stheven Cabral <stheven.cabral@gmail.com>
2020-02-23 12:16:56 -0800

    02232020  1216

--- js/app.js
+++ js/app.js
@@ -1,26 +1,35 @@
+// Traffic Chart
 Chart.defaults.global.defaultFontColor = 'darkgray';
 var traffic = document.getElementById('traffic-chart');
-traffic.height = 200;
-// Fix variable layout
+
 let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
         data: [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2000],
         backgroundColor: 'rgba(255,160,122, 0.4)',
         borderWidth: 1,
-        lineTension: 0
+        lineTension: 0,
+        pointBackgroundColor: 'rgb(255, 255, 255)',
+        pointBorderColor:  'rgba(255,160,122, 0.4)',
     }]
 };
 
 let trafficOptions = {
-    aspectRatio: 2.5,
+    responsive: true,
+    aspectRatio: 3,
     animation: {
         duration: 0
     },
     scales: {
+        xAxes: [{
+            ticks: {
+                fontSize: 8
+            }
+        }],
         yAxes: [{
             ticks: {
-                beginAtZero: true
+                beginAtZero: true,
+                fontSize: 8
             }   
         }]
     },
@@ -28,68 +37,77 @@ let trafficOptions = {
         display: false
     }
 };
-        
-    
-var daily = document.getElementById('daily-chart');
-const dailyData = {
-    labels: ["S", "M", "T", "W", "T", "F", "S"],
-    datasets: [{
-    label: '# of Hits',
-    data: [75, 115, 175, 125, 225, 200, 100],
-    backgroundColor: '#7477BF',
-    borderWidth: 1
-    }]
-    };
 
-const dailyOptions = {
-scales: {
-yAxes: [{
-ticks: {
-beginAtZero:true
-}
-}]
-},
-legend : {
-display: false
-}
-}
-    
-var mobile = document.getElementById('mobile-chart');
-const mobileData = {
-    labels: ["Desktop", "Tablet", "Phones"],
-    datasets: [{
-    label: '# of Users',
-    data: [2000, 550, 500],
-    borderWidth: 0,
-    backgroundColor: [
-    '#7477BF',
-    '#78CF82',
-    '#51B6C8'
-    ]
-    }]
-    };
-
-const mobileOptions = {
-    legend: {
-    position: 'right',
-    labels: {
-    boxWidth: 20,
-    fontStyle: 'bold'
-    }
-    }
-    }
-        
 var trafficChart = new Chart(traffic, {
     type: 'line',
     data: trafficData,
     options: trafficOptions
 });
+        
+// Daily Traffic Chart
+var daily = document.getElementById('daily-chart');
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: 'rgba(255,160,122, 0.4)',
+        borderWidth: 1,
+        barPercentage: 0.5
+    }]
+};
+
+const dailyOptions = {
+    scales: {
+        xAxes: [{
+            ticks: {
+                fontSize: 8
+            }
+        }],
+        yAxes: [{
+            ticks: {
+                beginAtZero:true,
+                fontSize: 8
+            }
+        }]
+    },
+    legend : {
+        display: false
+    }
+}
 
 var dailyChart = new Chart(daily, {
     type: 'bar',
     data: dailyData,
     options: dailyOptions
 });
+
+
+// Mobile Traffic Chart
+var mobile = document.getElementById('mobile-chart');
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            'rgb(255,160,122)',
+            '#78CF82',
+            '#51B6C8'
+        ]
+    }]
+};
+
+const mobileOptions = {
+    legend: {
+        position: 'right',
+        labels: {
+            boxWidth: 20,
+            fontStyle: 'normal'
+        }
+    }
+}
 
 var mobileChart = new Chart(mobile, {
     type: 'doughnut',
