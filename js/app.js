2c1d8ca2988fdab43466274679824580c4114106
Stheven Cabral <stheven.cabral@gmail.com>
2020-07-07 14:55:15 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ bellContainer.addEventListener('mouseout', function () {
 Chart.defaults.global.defaultFontColor = 'darkgray';
 var traffic = document.getElementById('traffic-chart');
 
+
 let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
